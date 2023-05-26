f4cd71131dc633d74bdc9fb4bb62277c28f9ed3e
Abdelaziz kissi <azizkissi29@gmail.com>
2023-05-26 10:42:29 +0100

    remove unused import

--- src/main/java/ma/ehei/gi4/librarymanagementapi/repository/BookRepository.java
+++ src/main/java/ma/ehei/gi4/librarymanagementapi/repository/BookRepository.java
@@ -2,7 +2,6 @@ package ma.ehei.gi4.librarymanagementapi.repository;
 
 import ma.ehei.gi4.librarymanagementapi.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
